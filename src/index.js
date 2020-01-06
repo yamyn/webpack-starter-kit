e3195d48c0292c61f911483deb77baa503fe6f22
Yaroslav Myn <ricky.month@gmail.com>
2020-01-07 00:53:12 +0200

    Made base config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import message from "./message";
+import "./style.css";
+
+console.log(message(2 + 5));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ],
+
+  devServer: {}
+
+  //   devtool: "cheap-eval-source-map"
+};
